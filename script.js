718a87f33c66f63d5b13fa89f2a3284d4414cc37
Volodia-SgS <vstekovics@gmail.com>
2024-09-09 04:31:27 +0200

    script.js

--- script.js
+++ script.js
@@ -3,28 +3,64 @@ function random(max){
 }
 
 function getComputerchoice(){
-    let a = random(3);
+    let ComputerChoice = random(3);
 
-    if (a == 0){
-        return "Rock"
+    if (ComputerChoice == 0){
+        ComputerChoice = "rock"
+        return ComputerChoice
     }
-    else if (a == 1){
-        return "Paper"
+    else if (ComputerChoice == 1){
+        ComputerChoice = "paper"
+        return ComputerChoice
     }
     else{
-        return "Scissors"
+        ComputerChoice = "scissors"
+        return ComputerChoice
     }
 }
 
-function getUserchoice(){
-    const value = window.prompt("Rock, Paper or Scissors ?")
-    console.log(value)
-    if (value != "Rock" || value != "Paper" || value != "Scissors"){
-        return window.prompt("Je vais te baiser choisis mieux !");
+function getHumanChoice(){
+    const HumanChoice = window.prompt("Rock, Paper or Scissors ?")
+    if (HumanChoice.toLowerCase() == "rock" || HumanChoice.toLowerCase() == "paper" || HumanChoice.toLowerCase() == "scissors"){
+        alert("You picked " + HumanChoice);
+        return HumanChoice.toLowerCase();
     } else{
-        return value;
+        return alert("Please enter a valid answer");
     }
 }
 
+let HumanScore = 0;
+let ComputerScore = 0;
 
-console.log(getUserchoice())
+
+
+function playRound(HumanChoice, ComputerChoice){
+    if (HumanChoice === ComputerChoice){
+        console.log("Tie !")
+    } else{
+        switch (HumanChoice){
+            case "rock":
+                if (ComputerChoice === "scissors"){
+                    console.log("You won the round !")
+                } else {
+                    console.log("You lost the round !")
+                }
+                break;
+            case "scissors":
+                if (ComputerChoice === "paper"){
+                    console.log("You won the round !")
+                } else {
+                    console.log("You lost the round !")
+                }
+                break;
+            case "paper":
+                if (ComputerChoice === "rock"){
+                    console.log("You won the round !")
+                } else {
+                    console.log("You lost the round !")
+                }
+                break;
+            }
+        }
+    }
+
